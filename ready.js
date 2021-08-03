fb19c81e5cc8a95c3b128c9f619be4546170f122
Glitch (shimmer-quilt-albacore) <none>
2021-08-03 14:55:20 +0000

    🏇🐜 Checkpoint ./ready.js:11806214/2121

--- ready.js
+++ ready.js
@@ -1,28 +1,15 @@
-const ayarlar = require('../ayarlar.json');
-let talkedRecently = new Set();
-module.exports = message => {
-  if (talkedRecently.has(message.author.id)) {
-    return;
-  }
-  talkedRecently.add(message.author.id);
-    setTimeout(() => {
-    talkedRecently.delete(message.author.id);
-  }, 2500);
-  let client = message.client;
-  if (message.author.bot) return;
-  if (!message.content.startsWith(ayarlar.prefix)) return;
-  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
-  let params = message.content.split(' ').slice(1);
-  let perms = client.elevation(message);
-  let cmd;
-  if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
-  }
-  if (cmd) {
-    if (perms < cmd.conf.permLevel) return;
-    cmd.run(client, message, params, perms);
-  }
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../.env');
 
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(`[BOT] | Botun destekçileri ve komutları başarıyla yüklendi, işlemler tamamlandı !`);
+  console.log(`[BOT] | (${client.user.username}) HAZIR KAPTAN!
+  ‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒`);
+  client.user.setStatus("online");
+  client.user.setActivity("Valorant,Steam,BluTV,Netflix ACCOUNT GENERATOR", { type: "WATCHING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
 };
+
